151a3c35a6ba028376ef85c9ff2bb00dd8def7fc
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-04-03 15:13:44 +0700

    display alamat kota

--- ActivityStructure(2)_182/ActivityStructure(2)_182.cpp
+++ ActivityStructure(2)_182/ActivityStructure(2)_182.cpp
@@ -22,5 +22,19 @@ int main()
 	Mahasiswa mhs;
 
 	cout << "masukan nim : ";
+	cin >> mhs.nim;
+	cout << "masukan nama : ";
+	cin >> mhs.nama;
+	cout << "Alamat" << endl;
+	cout << "\t desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t kota : ";
+	cin >> mhs.alamat.kota;
+	cout << "Masukan umur : ";
+	cin >> mhs.umur;
 
+	cout << "\nNim : " << mhs.nim;
+	cout << "\nNama : " << mhs.nama;
+	cout << "\ndesa : " << mhs.alamat.desa;
+	cout << "\nkota : " << mhs.alamat.kota;
 }
